3d0ffb703127270e287ad7e71e43fee6b4806bf6
F3L1XSSS <104171860+F3L1XSSS@users.noreply.github.com>
2024-04-14 16:41:48 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BusinessGame from './BuisnessGame'; // Основной код игры
 import LocationPage from './World2'; // Страница новой локации
+import SpacePage from './Space'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<BusinessGame />} />
         <Route path="/location" element={<LocationPage />} />
+        <Route path='/space' element={<SpacePage/>} />
       </Routes>
     </Router>
   );
