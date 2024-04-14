a2e4e4e928a80c9405ba63e326da2cb6dcbefa10
F3L1XSSS <104171860+F3L1XSSS@users.noreply.github.com>
2024-04-14 16:52:37 -0400

    Add files via upload

---
+++ my-app/src/Component/Location2.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import YellowCrystal from '../Photo/yellowCrystal.png'
+
+const Location = ({ id, name, cost, onPurchase, purchased }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/space');
+  };
+
+  return (
+    <>
+    <div key={id} className="bg-gray-600 p-4 rounded-lg shadow-inner">
+      <h3 className="text-lg">{name}</h3>
+      <p>Стоимость: {cost}  <img src={YellowCrystal} alt="Green Crystal" className="inline-block h-6 w-6" /></p>
+      {!purchased ? (
+        <button
+          onClick={() => onPurchase(id, cost)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3"
+        >
+          Купить
+        </button>
+      ) : (
+        <button
+          onClick={handleNavigate}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-3"
+        >
+          Перейти
+        </button>
+      )}
+    </div>   
+    </>
+  );
+};
+
+export default Location;
